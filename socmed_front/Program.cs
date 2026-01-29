f65ec0fb8dc39969d136712d58c623a2697b480d
untakersns <untaker1games@gmail.com>
2026-01-29 20:13:11 +0300

    2

--- socmed_front/Program.cs
+++ socmed_front/Program.cs
@@ -1,34 +1,11 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using socmed_front.Components;
-using socmed_front.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.WithOrigins("https://localhost:7013", "http://localhost:5242")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddScoped<ProtectedLocalStorage>();
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddScoped(sp=>
-    new HttpClient
-    {
-        BaseAddress = new Uri("https://localhost:7013/")
-    });
-builder.Services.AddAuthenticationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddCascadingAuthenticationState();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,9 +18,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowBlazorOrigin");
-app.UseAuthentication();
-app.UseAuthorization();
+
 app.UseAntiforgery();
 
 app.MapStaticAssets();
